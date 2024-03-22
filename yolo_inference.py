9a99eadf5395241e4dac843a37e46d883025654d
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2024-03-22 10:47:23 +0300

    Refactor yolo_inference.py to save prediction results in a text file

--- yolo_inference.py
+++ yolo_inference.py
@@ -9,13 +9,28 @@ def experiments(model, input_path, output_path, name):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using Device: ", device)
 
-    model.predict(input_path, save=True, save_txt=True,
-                  project=output_path, name=name, device=device)
+    result = model.predict(input_path, save=True,
+                           save_txt=True, project=output_path, name=name)
+    res = result[0]
+    classes = res.names
+
+    # open the file to write the results
+    with open(output_path + "output.txt", "a") as f:
+        f.write(f"\nModel: {name}\n")
+        for box in res.boxes:
+            conf = round(box.conf.item() * 100, 2)
+            xyxy = box.xyxy.tolist()[0]
+            cls = int(box.cls.item())
+            cls_name = classes.get(cls)
+            f.write(f"{cls_name} {conf} {xyxy}\n")
+
+    print("Done!")
 
 
 if __name__ == "__main__":
-    input_path = "input_data/videos/"
-    output_path = "runs/detect/exp1/"
+    # input_path = "input_data/shortest.mp4"
+    input_path = "input_data/images/image.png"
+    output_path = "runs/detect/test3/"
 
     models = {
         "yolov8x": YOLO("yolov8x.pt"),
